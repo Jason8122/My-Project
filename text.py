05ab48c992a0846df44990c9e84514d82f73975d
Jason8122 <kagirijason12@gmail.com>
2025-12-01 12:08:36 +0300

    Updated text

--- text.py
+++ text.py
@@ -1,25 +1,8 @@
-Menu = ["Pizza","Burger","Pasta"]
-print("Welcome to Peniel City Covenant Restaurant")
-print("Here's the menu")
-print("1.Pizza\n2.Burger\n3.Pasta")
-
-choice = int(input("Please select an order: "))
-print("You selected: " , choice)
-
-if choice == 1:
-    print("You ordered Pizza")
-elif choice == 2:
-    print("You ordered Burger")
-elif choice == 3:
-    print("You ordered Pasta")
-else:
-    print("Wrong order in menu, try again")
-
-if 1<= choice <= 3:
-    add_top = input("Do you want cheese (Yes/No): ")
-    if add_top.lower() == "yes":
-        print("Cheese added to your order")
+list = [1,2,3,4,5]
+for i in list:
+    if i == 4:
+        print(i)
     else:
-        print("No cheese added to your order")
-print("Here's your order")
-print("Thank you for dining with us!")    
+        print("Breaking at", i)
+        continue
+print("Loop ended")
