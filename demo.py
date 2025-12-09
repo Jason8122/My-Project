7dbeda9d2722ecd3e0db0f70d1ad10e22ef0d27a
Jason8122 <kagirijason12@gmail.com>
2025-12-09 12:29:41 +0300

    update demo

--- demo.py
+++ demo.py
@@ -7,5 +7,26 @@ def get_choices():
     choices = {"player": player_choice, "computer": computer_choice}
     return choices
 
+def check_winner(player,computer):
+    print(f"You chose {player}, computer chose {computer}")
+    if player == computer:
+        return "It's a tie!"
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock smashes scissors! You win!"
+        else:
+            return "Paper covers rock! You lose."
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers rock! You win!"
+        else:
+            return "Scissors cut paper! You lose."
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cut paper! You win!"
+        else:
+            return "Rock smashes scissors! You lose."
+
 choices = get_choices()
-print(choices)
+result = check_winner(choices["player"], choices["computer"])
+print(result)
